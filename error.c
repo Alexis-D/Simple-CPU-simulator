08b0aef8194867010973ce63f45aa21958248fc0
Alexis Daboville <alexis.daboville@gmail.com>
2011-06-07 23:44:05 +0200

    update de error.c

--- error.c
+++ error.c
@@ -1,34 +1,30 @@
-#include <stdlib.h>
+#include <stdio.h>
+
+#include "error.h"
+
+const char *error_names[] =
+{
+    "NOERRROR",
+    "UNKNOWN",
+    "ILLEGAL",
+    "CONDITION",
+    "IMMEDIATE",
+    "SEGTEXT",
+    "SEGDATA",
+    "SEGSTACK",
+};
+
+const char *warning_names[] =
+{
+    "HALT",
+}; 
 
 void error(Error err, unsigned addr){
-  
-  switch(err){
-    
-    case ERR_ILLEGAL:
-      printf("ERROR: %s reached at address %x\n","ILLEGAL ERROR",addr);
-      exit();
-    case ERR_CONDITION:
-      printf("ERROR: %s reached at address %x\n","CONDITION ERROR",addr);
-      exit();
-    case ERR_IMMEDIATE:
-      printf("ERROR: %s reached at address %x\n","IMMEDIATE ERROR",addr);
-      exit();
-    case ERR_SEGTEXT:
-      printf("ERROR: %s reached at address %x\n","TEXT SEGMENTATION ERROR",addr);
-      exit();
-    case ERR_SEGDATA:
-      printf("ERROR: %s reached at address %x\n","DATA SEGMENTATION ERROR",addr);
-      exit();
-    case ERR_SEGSTACK:
-      printf("ERROR: %s reached at address %x\n","STACK SEGMENTATION ERROR",addr);
-      exit();
-    default:
-      printf("ERROR: %s reached at address %x\n","UNKNOWN ERROR",addr);
-      exit();   
-  }
-
+    fprintf(stderr, "ERROR: %s reached at address 0x%x\n", error_names[err], addr);
+    exit(1);
 }
 
 void warning(Warning warn, unsigned addr){
-  printf("WARNING: HALT reached at address %x\n",addr);  
+    fprintf(stderr, "WARNING: %s reached at address 0x%x\n", warning_names[warn], addr);
 }
+
