0576f00560b53ed899d1f40c47fb9bdbbbdfa7f1
Alexis Daboville <alexis.daboville@gmail.com>
2011-06-10 15:05:31 +0200

    add some test, fib : ok

---
+++ tests/halt.asm
@@ -0,0 +1,20 @@
+
+//-----------------
+// Instructions
+//-----------------
+        TEXT 30
+
+        // Programme principal
+main    EQU *
+        NOP
+
+        END
+        
+//-----------------
+// Données et pile
+//-----------------
+        DATA 30
+        
+        WORD 1234
+        
+        END
